84c7ac65f49dcd868f37c6d1ff92167a378a73cf
Sandeep Sharma <sandeepksharma@hotmail.com>
2023-11-08 15:05:09 +0530

    . Solution Setup

---
+++ Source/Configuration/BlentOptions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Craft.Blent.Configuration;
+
+public class BlentOptions
+{
+    public BlentOptions(IServiceProvider serviceProvider, Action<BlentOptions> configureOptions)
+    {
+        Services = serviceProvider;
+        configureOptions?.Invoke(this);
+    }
+
+    /// <summary>
+    /// If true, the component that can control it's parent will automatically close it.
+    /// </summary>
+    public bool? AutoCloseParent { get; set; } = true;
+
+    /// <summary>
+    /// if true, the spin button will be shown on the numeric input component.
+    /// </summary>
+    public bool? ShowNumericStepButtons { get; set; } = true;
+
+    /// <summary>
+    /// If true, modal will keep focus inside it.
+    /// </summary>
+    public bool? ModalFocusTrap { get; set; } = true;
+
+    /// <summary>
+    /// Gets the service provider.
+    /// </summary>
+    public IServiceProvider Services { get; }
+}
---
+++ Source/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Craft.Blent.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCraftBlent(this IServiceCollection serviceCollection,
+        Action<BlentOptions>? configureOptions = null)
+    {
+        // If options handler is not defined we will get an exception so
+        // we need to initialize an empty action.
+        configureOptions ??= _ => { };
+
+        return serviceCollection;
+    }
+}
